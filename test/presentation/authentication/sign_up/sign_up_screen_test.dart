3eb8b200cacaef90967e17db214fc53d42002ee8
Eihror <igor_gmelo@hotmail.com>
2025-12-09 14:50:08 -0300

    Implement Unit tests

--- test/presentation/authentication/sign_up/sign_up_screen_test.dart
+++ test/presentation/authentication/sign_up/sign_up_screen_test.dart
@@ -1,8 +1,7 @@
 // ignore_for_file: discarded_futures
 
 import 'package:ecommerce_flutter/di/di.dart';
-import 'package:ecommerce_flutter/feature/authentication/presentation/navigation/authentication_routes.dart';
-import 'package:ecommerce_flutter/feature/authentication/presentation/sign_in/sign_in_screen.dart';
+import 'package:ecommerce_flutter/feature/authentication/presentation/sign_up/sign_up_screen.dart';
 import 'package:ecommerce_flutter/l10n/app_localizations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -28,16 +27,24 @@ void main() {
     GetIt.I.reset();
   });
 
+  late Finder userNameTextFormFieldFinder;
   late Finder userEmailTextFormFieldFinder;
   late Finder userPasswordTextFormFieldFinder;
-  late Finder signInButtonFinder;
+  late Finder userConfirmPasswordTextFormFieldFinder;
+  late Finder signUpButtonFinder;
 
   void registerAndValidateFields() {
-    signInButtonFinder = find.widgetWithText(
+    signUpButtonFinder = find.widgetWithText(
       FilledButton,
-      l10n.ui.buttonSignIn,
+      l10n.ui.buttonCreateAccount,
     );
-    expect(signInButtonFinder, findsOneWidget);
+    expect(signUpButtonFinder, findsOneWidget);
+
+    userNameTextFormFieldFinder = find.widgetWithText(
+      TextFormField,
+      l10n.ui.inputNameHint,
+    );
+    expect(userNameTextFormFieldFinder, findsOneWidget);
 
     userEmailTextFormFieldFinder = find.widgetWithText(
       TextFormField,
@@ -45,42 +52,53 @@ void main() {
     );
     expect(userEmailTextFormFieldFinder, findsOneWidget);
 
-    userPasswordTextFormFieldFinder = find.widgetWithText(
+    final passwordFinder = find.widgetWithText(
       TextFormField,
       l10n.ui.inputPasswordHint,
     );
-    expect(userPasswordTextFormFieldFinder, findsOneWidget);
+
+    expect(
+      passwordFinder,
+      findsAtLeast(2),
+    );
+
+    userPasswordTextFormFieldFinder = passwordFinder.at(0);
+    userConfirmPasswordTextFormFieldFinder = passwordFinder.at(1);
   }
 
-  testWidgets('When user tries to Sign In with valid account', (
+  testWidgets('When user tries to Sign Up with valid account', (
     WidgetTester tester,
   ) async {
     await tester.pumpWidget(
       CreateWidgetUnderTest(
         goRouter: mockRouter,
-        screenToTest: const SignInScreen(),
+        screenToTest: const SignUpScreen(),
       ),
     );
 
     await tester.pump();
 
-    l10n = appLocalizationsByWidgetContext(tester: tester, type: SignInScreen);
+    l10n = appLocalizationsByWidgetContext(tester: tester, type: SignUpScreen);
 
     registerAndValidateFields();
 
+    await tester.enterText(userNameTextFormFieldFinder, 'John Doe');
     await tester.enterText(
       userEmailTextFormFieldFinder,
       'john.doe@company.com',
     );
     await tester.enterText(userPasswordTextFormFieldFinder, '123abc@');
+    await tester.enterText(userConfirmPasswordTextFormFieldFinder, '123abc@');
 
-    await tester.tap(signInButtonFinder);
+    await tester.tap(signUpButtonFinder);
 
     await tester.pumpAndSettle(const Duration(seconds: 3));
 
     final possibleErrorList = [
+      l10n.ui.errorInvalidName,
       l10n.ui.errorInvalidEmail,
       l10n.ui.errorInvalidPassword,
+      l10n.ui.errorInvalidConfirmPassword,
     ];
 
     errorFoundList = possibleErrorList.where((errorMessage) {
@@ -89,6 +107,6 @@ void main() {
 
     expect(errorFoundList, isEmpty);
 
-    verifyNever(() => mockRouter.go(SignUpRoute().path));
+    verify(mockRouter.pop).called(1);
   });
 }
